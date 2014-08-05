56c85f9407b5b62e51be50d14959972eb50e68b9
Lawrence Siebert <gryftir@gmail.com>
2014-08-04 21:48:08 -0700

    created function stubs in bloom.c based on declarations in bloom.h

--- bloom.c
+++ bloom.c
@@ -3,4 +3,40 @@
 char * init (void) {
 	return	setlocale (LC_ALL, "");
 }
+
 	
+BLOOM * new_bloom(int element_size_bits, long num_elements, char * encoding){
+	
+	return;
+}
+
+int destroy_bloom(BLOOM * bloom_array){
+	
+	return;
+}
+
+int found(BLOOM * bloom_array, SEARCH * string){
+	
+	return;
+}
+
+SEARCH * new_search(void * string, char * encoding, int char_size){
+	
+	return;
+}
+
+SEARCH * new_search_encoding(SEARCH * string, char * encoding){
+	
+	return;
+}
+
+SEARCH * copy_search(SEARCH * string){
+	
+	return;
+}
+
+int destroy_search(SEARCH * string){
+	
+	return;
+}
+
