9834fc2642a64a4b842a5037acd50cdaa9bd0106
Lawrence Siebert <gryftir@gmail.com>
2014-08-16 00:19:50 -0700

    crated tests for new_string_struct and a preliminary test for add_to_bloom

--- bloom_example.c
+++ bloom_example.c
@@ -1,11 +1,18 @@
 #include "bloom.h"
 
-#define WORDFILE "/usr/share/dict/american-english-insane" 
-#define SEARCHFILE "search.txt"
+int main(int argc, char ** argv) {
+	uint32_t * i = NULL;
+	BLOOM * b = new_bloom(32, 10, UTF8);
+	for( i = (uint32_t *) (b->array); i <  (uint32_t *)(b->array + (sizeof(uint32_t) * b->num_elements)); i++){
+		printf("%"PRIu32"\n", *i); 
+	}
+	printf("\n\n");
+	char * string = "ᴁ test";
+	add_to_bloom(b, string);
+	for(i = (uint32_t *) (b->array); i <  (uint32_t *)(b->array + (sizeof(uint32_t) * b->num_elements)); i++){
+		printf("%"PRIu32"\n", *i); 
+	}
+	destroy_bloom(b);
+		return 0;
 
-
-int main(int argc, char *argv[])
-{
-		
-	return 0;
 }
